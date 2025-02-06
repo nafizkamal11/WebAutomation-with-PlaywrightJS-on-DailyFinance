a511f36d82d78545f16a12c117cfceed1aeaf2d9
Nafiz Kamal <nfzkml11.cs@gmail.com>
2025-02-06 19:27:07 +0600

    add allure report support

--- playwright.config.js
+++ playwright.config.js
@@ -22,9 +22,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+      reporter: "allure-playwright",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   expect: {
     timeout: 10000, // Default timeout for visibility and other assertions
